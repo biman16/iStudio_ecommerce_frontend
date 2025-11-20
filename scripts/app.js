8a23aaf0469a3e1a42a0b916d0e3952f2b24b4d9
Biman Chandra Das <bimandas1611@gmail.com>
2025-11-21 01:59:04 +0530

    Added banner and edited hero section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 console.log("E-Commerce Website Loaded");
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navigationMenu = document.querySelector('.navigation-menu');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navigationMenu.classList.toggle('active');
+    });
+});
